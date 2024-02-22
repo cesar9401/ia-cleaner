128ca9180170ab19518cc10ccf6b92cbe71ef21b
César Tzoc <csartalva01@gmail.com>
2024-02-22 04:36:15 -0600

    feat: moving, agai

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 from model.board import Board
 from model.cleaner import Cleaner
 
-board = Board(4, 5)
+board = Board(3, 3)
 # board.print_board()
 
 cleaner = Cleaner()
 
 # board.print_board()
 
-
-def game():
-    print('here', end='\n')
+def game(cleaner):
+    print(end='\n')
     cur_x, cur_y = cleaner.cur_x, cleaner.cur_y
     right, down = cleaner.right, cleaner.down
-    print(f'x: {cur_x}, y: {cur_y}')
 
+    print(f'x: {cur_x}, y: {cur_y}')
+    print(f'right: {right}, down: {down}')
     board.board[cur_x][cur_y].set_cleaner(cleaner)
     board.print_board()
     board.board[cur_x][cur_y].remove_cleaner()
@@ -23,74 +23,79 @@ def game():
     if right and len(board.board) - 1 > cur_x:
         cleaner.cur_x = cur_x + 1
         cleaner.cur_y = cur_y
-    elif not right and len(board.board) - 1 < cur_x:
+    elif not right and cur_x > 0:
         cleaner.cur_x = cur_x - 1
         cleaner.cur_y = cur_y
     elif right and len(board.board) - 1 == cur_x:
-        cleaner.cur_x = cleaner.cur_x
-        cleaner.cur_y = cur_y + 1
-        cleaner.right = not cleaner.right
+        if len(board.board[cur_x]) - 1 == cur_y:
+            cleaner.cur_x = cur_x - 1
+            cleaner.cur_y = cur_y
+            cleaner.right = not right
+            cleaner.down = not down
+        else:
+            cleaner.cur_x = cur_x
+            cleaner.right = not right
+            if down:
+                cleaner.cur_y = cur_y + 1
+            else:
+                cleaner.cur_y = cur_y - 1
     elif not right and cur_x == 0:
-        cleaner.cur_x = cur_x + 1
-        cleaner.cur_y = cur_y
-        cleaner.right = not cleaner.right
+        if len(board.board[cur_x]) - 1 == cur_y:
+            cleaner.cur_x = cur_x
+            cleaner.cur_y = cur_y - 1
+            cleaner.right = not right
+        else:
+            cleaner.cur_x = cur_x
+            cleaner.cur_y = cur_y + 1
+            cleaner.right = not right
     elif down and len(board.board[cur_x]) - 1 > cur_y:
         cleaner.cur_x = cur_x
         cleaner.cur_y = cur_y + 1
-    elif not down and len(board.board[cur_x]) - 1 < cur_y:
+    elif not down and cur_y > 0:
         cleaner.cur_x = cur_x
         cleaner.cur_y = cur_y - 1
+        print('down 1')
     elif down and len(board.board[cur_x]) - 1 == cur_y:
-        cleaner.cur_y = cleaner.cur_y
+        cleaner.cur_y = cur_y
         cleaner.cur_x = cur_x + 1
-        cleaner.down = not cleaner.down
+        cleaner.down = not down
+        print('down 2')
     elif not down and cur_y == 0:
-        cleaner.cur_y = cur_y + 1
-        cleaner.cur_x = cur_x
-        cleaner.down = not cleaner.down
+        cleaner.cur_y = cur_y
+        cleaner.cur_x = cur_x + 1
+        cleaner.down = not down
     else:
         print('else')
 
-
-    # cleaner.print()
-    # if right and len(board.board) - 1 > cur_x:
-    #     cleaner.cur_x = cur_x + 1
-    #     cleaner.cur_y = cur_y
-    # elif not right and len(board.board) - 1 <= cur_x:
-    #     cleaner.cur_x = cur_x - 1
-    #     cleaner.cur_y = cur_y
-    # elif down and len(board.board[cur_x]) - 1 > cur_y:
-    #     cleaner.cur_x = cur_x
-    #     cleaner.cur_y = cur_y + 1
-    # elif not down and len(board.board[cur_x]) - 1 <= cur_y:
-    #     cleaner.cur_x = cur_x
-    #     cleaner.cur_y = cur_y - 1
-    # elif right and len(board.board[cur_x]) - 1 == cur_y:
-    #     cleaner.cur_y = cur_y
-    #     cleaner.cur_x = cur_x + 1
-    #     cleaner.down = not cleaner.down
-    # elif not right and cur_y == 0:
-    #     cleaner.cur_y = cur_y
-    #     cleaner.cur_x = cur_x - 1
-    #     cleaner.down = not cleaner.down
-    # elif down and len(board.board) - 1 == cur_x:
-    #     cleaner.cur_y = cur_y + 1
-    #     cleaner.cur_x = cur_x
-    #     cleaner.right = not cleaner.right
-    # elif not down and cur_x == 0:
-    #     cleaner.cur_y = cur_y - 1
-    #     cleaner.cur_x = cur_x
-    #     cleaner.right = not cleaner.right
-    # else:
-    #     print('else')
-
-
-game()
-game()
-game()
-game()
-game()
-game()
-game()
-game()
-game()
+game(cleaner)
+game(cleaner)
+game(cleaner)
+game(cleaner)
+game(cleaner)
+game(cleaner)
+game(cleaner)
+game(cleaner)
+game(cleaner)
+game(cleaner)
+game(cleaner)
+game(cleaner)
+game(cleaner)
+game(cleaner)
+game(cleaner)
+game(cleaner)
+game(cleaner)
+game(cleaner)
+game(cleaner)
+game(cleaner)
+game(cleaner)
+game(cleaner)
+game(cleaner)
+game(cleaner)
+game(cleaner)
+game(cleaner)
+game(cleaner)
+game(cleaner)
+game(cleaner)
+game(cleaner)
+game(cleaner)
+game(cleaner)
